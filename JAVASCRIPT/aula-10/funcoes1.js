a7deed7898b08fd9cc1542e48c5bac02d783a8f3
Arthur Martins Alves <oarthurma@gmail.com>
2025-07-10 20:20:27 -0300

    inicio do arquivo de funcoes

--- JAVASCRIPT/aula-10/funcoes1.js
+++ JAVASCRIPT/aula-10/funcoes1.js
@@ -1,9 +1,24 @@
-// Definição de função
-function saudacao(nomeDoEstudante, curso) {
-  // template string || template literals
-
-  console.log(`Olá, ${nomeDoEstudante}!`);
-  console.log(`Olá, seja bem-venda(o) ao curso de de ${curso}`);
+// Definição da função
+function saudacao(nomeDoEstudante, curso = "Front-end em React") { 
+  // template sring | template literária
+  return `Olá, ${nomeDoEstudante}! Seja bem-vinda(o) ao curso de ${curso}!`;
 }
 
-saudacao("Walisson", "Back-end em Node.js"); // chamada da função saudacao
+const mensagemDaSaudacao = saudacao("Walisson", "Back-end em Node.js"); // chamada da função saudação
+
+console.log(mensagemDaSaudacao);
+
+// * Exemplo 2
+
+console.clear();
+
+function somar(numero1, numero2) {
+  return (numero1 + numero2)
+}
+
+const resultado = somar(2, 3)
+
+console.log(resultado);
+console.log(resultado / 10);
+
+// ! 26:45
