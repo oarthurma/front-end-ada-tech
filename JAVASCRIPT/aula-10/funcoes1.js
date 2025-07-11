5dbae2a9d3c10412f81d00223c9a3ad410043e77
Arthur Martins Alves <oarthurma@gmail.com>
2025-07-11 05:07:17 -0300

    avanço na aula sobre funções

--- JAVASCRIPT/aula-10/funcoes1.js
+++ JAVASCRIPT/aula-10/funcoes1.js
@@ -21,4 +21,26 @@ const resultado = somar(2, 3)
 console.log(resultado);
 console.log(resultado / 10);
 
-// ! 26:45
+console.clear();
+
+// ---------------------------------
+// FUNÇÕES ANÔNIMAS
+
+const dobroDoNumero = function (numero) {
+  return numero * 2;
+};
+
+const dobro = dobroDoNumero(2)
+console.log(dobro)
+
+// -----------------------------
+// ARROW FUNCTIONS: Função Seta
+
+const subtrair = (numero1, numero2) => {
+  return numero1 - numero2;
+};
+
+const multiplicar = (numero1, numero2) => numero1 * numero2;
+
+const triploDoNumero = numero => numero * 3
+
--- JAVASCRIPT/aula-10/objetos.js
+++ JAVASCRIPT/aula-10/objetos.js
@@ -1,52 +1,14 @@
 // Object Literal
 
-const array = ["Walisson", 27, 1.77, true];
+const array = [ 'Walisson', 27, 1.77, true, ];
 
 const pessoa = {
-  nome: "Walisson",
-  idade: 27,
-  altura: 1.77,
-  ehProgramador: true,
-  hobbies: ["Jogar FIFA", "Academia", "Ler"],
-  imprimirAlgo: () => {
-    console.log("Chamou a função!");
-  },
-};
 
-console.log(pessoa);
-console.clear();
+    nome: 'Walisson',
+    idade: 27,
+    altura: 1.77,
+    ehProgramador: true,
 
-console.log(pessoa.nome);
-console.log(pessoa.hobbies);
-console.log(pessoa["idade"]);
+}
 
-pessoa.profissao = "Desenvolvidor";
-
-pessoa.nome = "Walisson Silva";
-
-console.log(pessoa);
-
-delete pessoa.altura;
-
-console.log(pessoa);
-
-console.clear();
-
-pessoa.imprimirAlgo();
-
-const idade = 27;
-const altura = 1.77;
-
-const objeto = {
-  idade,
-  altura,
-};
-
-console.log(objeto);
-console.clear();
-
-const { nome, hobbies } = pessoa;
-
-console.log(nome);
-console.log(hobbies);
-console.log(pessoa);
+// ! 47:59
