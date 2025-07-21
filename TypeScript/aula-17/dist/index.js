0a056fe8d7ab3695a38da8261212e3e5abdde669
Arthur Martins Alves <oarthurma@gmail.com>
2025-07-21 05:53:00 -0300

    agora é POO

--- TypeScript/aula-17/dist/index.js
+++ TypeScript/aula-17/dist/index.js
@@ -1,5 +1,56 @@
 "use strict";
+let numero = 20; // inferência de tipo
+const pi = 3.1415;
+let nome = "Walisson";
+let correta = true;
+let resultado = numero * pi;
+// number[] or Array<number>
+const numeros = [1, 2, 3, 4, 5];
+// ! const misto: any[] = ['Walisson', 27, 1.77, {}, {} => {}, true]
+// ! Evite ao máximo utilizar o any!
+const idades = [];
+idades.push(23);
+idades.push(38);
+idades.push(12);
+idades.push(49);
+idades.push(16);
+const menoresDeIdade = idades.filter((idade) => idade < 18);
+console.log(menoresDeIdade);
+const pessoaTupla = ["Walisson", 27]; // tupla (tuple)
+const pessoa = {
+    nome: "Walisson",
+    idade: 27,
+    altura: 1.77,
+};
+pessoa.profissao = "Desenvolvedor";
+console.log(pessoa);
+function chooseNumber(numero1, numero2, criterio) {
+    switch (criterio) {
+        case "greater":
+            return numero1 > numero2 ? numero1 : numero2;
+        case "lower":
+            return numero1 < numero2 ? numero1 : numero2;
+        default:
+            const numeroAleatorio = Math.random();
+            if (numeroAleatorio >= 0.5)
+                return numero1;
+            return numero2;
+    }
+}
+const numeroEscolhido = chooseNumber(10, 20, "lower");
+console.log("numeroEscolhido:", numeroEscolhido);
 function somar(num1, num2) {
     return num1 + num2;
 }
-somar(10, 20);
+const pessoas = {
+    "123.456.789-00": {
+        nome: "Fulano",
+        idade: 20,
+        altura: 1.89,
+    },
+    "123.456, 789-01": {
+        nome: "Fulana",
+        idade: 19,
+        altura: 1.72,
+    },
+};
--- TypeScript/aula-17/index.ts
+++ TypeScript/aula-17/index.ts
@@ -8,8 +8,120 @@ let correta: boolean = true;
 
 let resultado = numero * pi;
 
-const nomeDoUsuario = prompt("Qual é o seu nome?");
+// number[] or Array<number>
+const numeros: number[] = [1, 2, 3, 4, 5];
 
-console.log(nomeDoUsuario);
+// ! const misto: any[] = ['Walisson', 27, 1.77, {}, {} => {}, true]
+// ! Evite ao máximo utilizar o any!
 
-// ! 11:30
+const idades: number[] = [];
+idades.push(23);
+idades.push(38);
+idades.push(12);
+idades.push(49);
+idades.push(16);
+
+const menoresDeIdade = idades.filter((idade) => idade < 18);
+
+console.log(menoresDeIdade);
+
+const pessoaTupla: [string, number] = ["Walisson", 27]; // tupla (tuple)
+
+// Object Types
+
+// interface
+// tipo costumizado
+// interface Person {
+//   nome: string;
+//   idade: number;
+//   profissao?: string;
+//   altura: number;
+// }
+
+type Person = {
+  nome: string;
+  idade: number;
+  altura: number;
+  profissao?: string;
+};
+
+type MyString = string;
+
+const pessoa: Person = {
+  nome: "Walisson",
+  idade: 27,
+  altura: 1.77,
+};
+
+pessoa.profissao = "Desenvolvedor";
+
+console.log(pessoa);
+
+// type aliases
+type Criterio = "greater" | "lower";
+
+function chooseNumber(
+  numero1: number,
+  numero2: number,
+  criterio?: Criterio
+): number {
+  switch (criterio) {
+    case "greater":
+      return numero1 > numero2 ? numero1 : numero2;
+    case "lower":
+      return numero1 < numero2 ? numero1 : numero2;
+    default:
+      const numeroAleatorio = Math.random();
+
+      if (numeroAleatorio >= 0.5) return numero1;
+      return numero2;
+  }
+}
+
+const numeroEscolhido = chooseNumber(10, 20, "lower");
+
+console.log("numeroEscolhido:", numeroEscolhido);
+
+function somar(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+// * Utility Types: A ideia deles é permitir que você crie novos tipos a partir
+// *de tipos já existentes
+
+// 1.Partial
+
+type PersonPartial = Partial<Person>;
+
+// 2. Required
+
+type PersonRequired = Required<Person>;
+
+// 3. Pick
+
+type PersonPicked = Pick<Person, "nome" | "idade">;
+
+// 4. Omit
+
+type PersonOmit = Omit<Person, "profissao">;
+
+// 5. Exclude
+
+type CriterioExclude = Exclude<Criterio, "greater">;
+
+// 6. Record
+
+type Pessoas = Record<string, Person>;
+
+const pessoas: Pessoas = {
+  "123.456.789-00": {
+    nome: "Fulano",
+    idade: 20,
+    altura: 1.89,
+  },
+  "123.456, 789-01": {
+    nome: "Fulana",
+    idade: 19,
+    altura: 1.72,
+  },
+};
