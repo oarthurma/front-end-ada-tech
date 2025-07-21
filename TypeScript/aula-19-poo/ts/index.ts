a9266b0fa49d5ac64744b392e404874d86e933ba
oarthurma <oarthurma@gmail.com>
2025-07-21 14:03:50 -0300

    PC da cx - poo 37:53

---
+++ TypeScript/aula-19-poo/ts/index.ts
@@ -0,0 +1,35 @@
+// Aula 19 - POO
+
+// Classe: abstração
+// Definindo a abstração de uma pessoa
+class Pessoa {
+  // atributos: cararísticas
+  nome: string;
+  idade: number;
+  altura: number;
+
+  // métodos: ação (funções)
+
+  // Método construtor
+  constructor(nome: string, idade: number, altura: number) {
+    this.nome = nome; // this = este
+    this.idade = idade;
+    this.altura = altura;
+  }
+
+  dormir() {
+    console.log("Dormindo... 😴");
+  }
+}
+
+// Criando/instanciando uma pessoa (indivíduo/objeto) a partir da definição da classe Pessoa
+const pessoa1 = new Pessoa("Walisson", 27, 1.77);
+const pessoa2 = new Pessoa("Pessoa 2", 30, 1.89);
+
+console.log(pessoa1);
+console.log(pessoa1.nome);
+
+console.log(pessoa2);
+pessoa2.dormir();
+
+// ! 37:53
