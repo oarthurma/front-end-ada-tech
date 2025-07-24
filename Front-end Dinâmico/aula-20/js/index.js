6b997d37dc5893a18dc8d33c678ab1fb69746ce6
Arthur Martins Alves <oarthurma@gmail.com>
2025-07-24 02:24:43 -0300

    inicio DOM

---
+++ Front-end Dinâmico/aula-20/js/index.js
@@ -0,0 +1,13 @@
+// Selecionar elementos HTML da nossa página utilizando o document
+
+//1. Pelo nome da tag
+const todosP = document.getElementsByTagName("p");
+
+console.log(todosP);
+
+// 2. Pelo nome da classe
+const todosClasseParagrafo = document.getElementsByClassName("paragrafo");
+
+console.log(todosClasseParagrafo);
+
+// ! 27:40
