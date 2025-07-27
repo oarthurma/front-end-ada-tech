feb077cc3846f91fefad5e5b2868e4edea311d2f
Arthur Martins Alves <oarthurma@gmail.com>
2025-07-27 00:28:43 -0300

    aula sobre assincronismo reassistida

--- JAVASCRIPT/aula-13/assincronismo.js
+++ JAVASCRIPT/aula-13/assincronismo.js
@@ -21,12 +21,60 @@ const fs = require("fs");
 
 // Exemplo 02: setTimeout(função que cria um timer)
 
-console.log("Antes do setTimeout...");
+// console.log("Antes do setTimeout...");
 
-setTimeout(() => {
-  console.log("Isso aqui vai ser executado após 2 segundos!");
-}, 2 * 1000);
+// setTimeout(() => {
+//   console.log("Isso aqui vai ser executado após 2 segundos!");
+// }, 2 * 1000);
 
-console.log("DEPOIS do setTimelime...");
+// console.log("DEPOIS do setTimelime...");
 
-// ! 30:30
+// * 2. Promises (promessas)
+
+// console.log("Antes da criação d promise...");
+
+function lerArquivoPromise() {
+  return new Promise((resolve, reject) => {
+    fs.readFile("aula-13/arquivo.txt", (erro, conteudoDoArquivo) => {
+      if (erro) {
+        reject("Ocorreu um erro ao tentar ler o arquivo:", erro);
+      } else {
+        resolve(String(conteudoDoArquivo));
+      }
+    });
+  });
+}
+
+// Foco a partir daqui...
+// lerArquivoPromese()
+//   .then((retornoDoResolveDaPromise) => {
+//     console.log("Deu certo:", retornoDoResolveDaPromise);
+//   })
+//   .catch((erro) => {
+//     console.log("Deu ruim:", erro);
+//   })
+//   .finally(() => {
+//     console.log(
+//       "Isso aqui vai ser executado, independente do sucesso ou fracasso da promessa, no final dela. "
+//     );
+//   });
+
+// * 3. async/await
+
+async function leituraDeDados() {
+  console.log("Isso é executado antes da promise ser resolvida!");
+
+  try {
+    const dadosDoArquivoLido = await lerArquivoPromise(); // espera a promessa ser resolvida para avançar no código
+
+    console.log(dadosDoArquivoLido);
+    console.log("Isso é executado DEPOIS da promise ser resolvida!");
+  } catch (err) {
+    console.log(err);
+    console.log("Isso aqui é executado depois da promise (com erro).");
+  } finally {
+    console.log("Dentro do finally!");
+  }
+}
+
+leituraDeDados();
---
+++ JAVASCRIPT/aula-13/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+console.log("Antes do setTimeout...");
+
+// setTimeout(() => {
+//   console.log("Isso aqui vai ser executado após 2 segundos!");
+// }, 2 * 1000);
+
+// console.log("DEPOIS do setTimelime...");
