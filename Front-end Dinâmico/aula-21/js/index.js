69ae60da71ef9c5834ee7f543c41a6bcd801f256
Arthur Martins Alves <oarthurma@gmail.com>
2025-08-03 05:39:02 -0300

    aula 21 finalizada

---
+++ Front-end Dinâmico/aula-21/js/index.js
@@ -0,0 +1,79 @@
+const botaoAumentar = document.querySelector("#aumentar-botao");
+const botaoDiminuir = document.querySelector("#diminuir-botao");
+
+const contadorElemento = document.querySelector("#contador");
+
+const input = document.querySelector("#input");
+
+botaoAumentar.addEventListener("click", (event) => {
+  const valorAtual = Number(contadorElemento.textContent);
+
+  contadorElemento.textContent = valorAtual + 1;
+
+  botaoAumentar.classList.add("btn");
+  botaoDiminuir.classList.remove("btn");
+});
+
+botaoDiminuir.addEventListener("click", (event) => {
+  const valorAtual = Number(contadorElemento.textContent);
+
+  contadorElemento.textContent = valorAtual - 1;
+
+  botaoDiminuir.classList.add("btn");
+  botaoAumentar.classList.remove("btn");
+});
+
+input.addEventListener("input", () => {
+  console.log(input.value);
+});
+
+// Adicionando estilos inline no elemento contador
+
+contadorElemento.style.color = "red";
+contadorElemento.style.padding = "0 2rem";
+contadorElemento.style.border = "1px solid #AAA";
+contadorElemento.style.width = "150px";
+
+// Manipular classes
+
+console.log(botaoDiminuir.classList);
+
+botaoAumentar.classList.add("btn");
+botaoDiminuir.classList.remove("btn");
+
+const themeButton = document.querySelector("#theme");
+
+let darkTheme;
+
+// Definindo uma função que será executada ao carregar o conteúdo da página/janela
+window.onload = () => {
+  const inDarkThemeStorage = localStorage.getItem("isDarkTheme");
+
+  darkTheme = inDarkThemeStorage === "true";
+
+  const body = document.querySelector("body");
+
+  if (darkTheme) {
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
+  } else {
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
+  }
+};
+
+themeButton.addEventListener("click", () => {
+  darkTheme = !darkTheme;
+
+  localStorage.setItem("isDarkTheme", darkTheme);
+
+  const body = document.querySelector("body");
+
+  if (darkTheme) {
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
+  } else {
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
+  }
+});
