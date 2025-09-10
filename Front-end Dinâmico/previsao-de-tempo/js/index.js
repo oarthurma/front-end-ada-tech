435960b4359e2d3987a27ff89a37c329c82299af
Arthur Martins Alves <oarthurma@gmail.com>
2025-09-10 07:04:23 -0300

    conclusão do projeto de previsão do tempo com API fetch

--- Front-end Dinâmico/previsao-de-tempo/js/index.js
+++ Front-end Dinâmico/previsao-de-tempo/js/index.js
@@ -1,10 +1,35 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/*
-30:50
-
-https://api.openweathermap.org/data/2.5/weather?q=sao%20paulo&appid=3d828a0df5c454699fc9f39ae8bb92d6&lang=pt_br&units=metric
-
-https://openweathermap.org/img/wn/01d@2x.png
-*/
-//# sourceMappingURL=index.js.map
+const form = document.querySelector("#search-form > form");
+const input = document.querySelector("#input-localizacao");
+const sectionTempoInfo = document.querySelector("#tempo-info");
+form?.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    if (!input || !sectionTempoInfo)
+        return;
+    const localizacao = input.value;
+    if (localizacao.length < 3) {
+        alert("O local precisa ter, pelo menos, 3 letras.");
+        return;
+    }
+    try {
+        const resposta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${localizacao}&appid=3d828a0df5c454699fc9f39ae8bb92d6&lang=pt_br&units=metric`);
+        const dados = await resposta.json();
+        const infos = {
+            temperatura: Math.round(dados.main.temp),
+            local: dados.name,
+            icone: `https://openweathermap.org/img/wn/${dados.weather[0].icon}@2x.png`,
+        };
+        sectionTempoInfo.innerHTML = `
+    <div class="tempo-dados">
+            <h2>${infos.local}</h2>
+  
+            <span>${infos.temperatura}ºC</span>
+          </div>
+  
+          <img src="${infos.icone}" / >
+    `;
+    }
+    catch (err) {
+        console.log("Deu um erro na obtenção dos dados da API.", err);
+    }
+});
